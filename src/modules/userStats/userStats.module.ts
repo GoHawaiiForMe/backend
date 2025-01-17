b32e6d6705d06603ab54318219bf1ce5a03aae04
Maybeiley <2784519@gmail.com>
2025-01-17 17:39:47 +0900

    Feat: #73 UserStats 모델 import 추가

--- src/modules/userStats/userStats.module.ts
+++ src/modules/userStats/userStats.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import UserStatsController from './userStats.controller';
 import UserRepository from '../user/user.repository';
 import UserStatsService from './userStats.service';
+import UserStatsSchema, { UserStats } from 'src/providers/database/mongoose/userStats.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: UserStats.name, schema: UserStatsSchema }])],
   controllers: [UserStatsController],
   providers: [UserRepository, UserStatsService],
   exports: []
