4098b42560753529c505f60135f723185f944cc2
Maybeiley <2784519@gmail.com>
2025-01-17 17:27:38 +0900

    Feat: #73 UserStats 모듈 등록

--- src/modules/userStats/userStats.module.ts
+++ src/modules/userStats/userStats.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import UserStatsController from './userStats.controller';
+import UserRepository from '../user/user.repository';
+import UserStatsService from './userStats.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [UserStatsController],
+  providers: [UserRepository, UserStatsService],
   exports: []
 })
 export default class UserStatsModule {}
