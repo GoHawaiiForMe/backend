83f1879070888ceb8a0e7456bf91954d05c03b74
TaeyeonKim <gggm0117@naver.com>
2025-01-17 20:13:35 +0900

    Fix: #60 주석 제거및 취소

--- src/providers/portOne/portone.provider.ts
+++ src/providers/portOne/portone.provider.ts
@@ -1,13 +1,13 @@
-// import { Payment } from '@portone/server-sdk/dist/generated/payment';
+import { Payment } from '@portone/server-sdk/dist/generated/payment';
 
-// export interface PortOneProvider {
-//   getPayment(paymentId: string): Promise<Payment | null>;
-//   cancelPayment(paymentId: string, reason: string): Promise<boolean>;
-// }
+export interface PortOneProvider {
+  getPayment(paymentId: string): Promise<Payment | null>;
+  cancelPayment(paymentId: string, reason: string): Promise<boolean>;
+}
 
-// export class GetPaymentError extends Error {
-//   constructor(message: string) {
-//     super(message);
-//     this.name = 'PaymentError';
-//   }
-// }
+export class GetPaymentError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'PaymentError';
+  }
+}
