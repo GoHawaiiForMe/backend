28c9042f02e914eb6526acdda229fb399444b330
Maybeiley <2784519@gmail.com>
2025-02-17 17:45:05 +0900

    Feat: #227 Follow 모듈 e2e 테스트 코드

--- src/modules/follow/follow.controller.ts
+++ src/modules/follow/follow.controller.ts
@@ -4,16 +4,17 @@ import { UserId } from 'src/common/decorators/user.decorator';
 import { Role } from 'src/common/decorators/roleGuard.decorator';
 
 @Controller('follow')
-@Role('DREAMER')
 export default class FollowController {
   constructor(private readonly service: FollowService) {}
   @Post()
+  @Role('DREAMER')
   @HttpCode(HttpStatus.NO_CONTENT)
   async follow(@UserId() dreamerId: string, @Body('makerId') makerId: string): Promise<null> {
     return await this.service.create(dreamerId, makerId);
   }
 
   @Delete()
+  @Role('DREAMER')
   @HttpCode(HttpStatus.NO_CONTENT)
   async unfollow(@UserId() dreamerId: string, @Body('makerId') makerId: string): Promise<null> {
     return await this.service.delete(dreamerId, makerId);
