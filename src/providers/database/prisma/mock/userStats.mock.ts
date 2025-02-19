2ba870cc3c9a31d2c91568ee6a8a379cd5c6582c
Maybeiley <2784519@gmail.com>
2025-02-19 12:08:48 +0900

    Feat: #237 UserStats 목데이터 추가

--- src/providers/database/prisma/mock/userStats.mock.ts
+++ src/providers/database/prisma/mock/userStats.mock.ts
@@ -7,63 +7,63 @@ export const USER_STATS = [
     totalConfirms: 32
   },
   {
-    id: 'ef846519-2b73-4be4-807e-f6ef1c07eb60',
+    userId: 'ef846519-2b73-4be4-807e-f6ef1c07eb60',
     averageRating: 0,
     totalReviews: 0,
     totalFollows: 2,
     totalConfirms: 0
   },
   {
-    id: 'a08d9856-adfa-4f83-9e9a-48401f3d0ef3',
+    userId: 'a08d9856-adfa-4f83-9e9a-48401f3d0ef3',
     averageRating: 0,
     totalReviews: 0,
     totalFollows: 1,
     totalConfirms: 6
   },
   {
-    id: '6b4ded82-e680-410d-b86d-ce6974e53f37',
+    userId: '6b4ded82-e680-410d-b86d-ce6974e53f37',
     averageRating: 0,
     totalReviews: 0,
     totalFollows: 1,
     totalConfirms: 8
   },
   {
-    id: '49be61b8-0905-448a-8534-fe5b1a0004c2',
+    userId: '49be61b8-0905-448a-8534-fe5b1a0004c2',
     averageRating: 0,
     totalReviews: 0,
     totalFollows: 2,
     totalConfirms: 0
   },
   {
-    id: '2555fe16-1649-48c8-8f3f-971b87dd34a7',
+    userId: '2555fe16-1649-48c8-8f3f-971b87dd34a7',
     averageRating: 0,
     totalReviews: 0,
     totalFollows: 0,
     totalConfirms: 0
   },
   {
-    id: 'f0f6b3d9-529a-41cc-8ca1-3c29d0a3670a',
+    userId: 'f0f6b3d9-529a-41cc-8ca1-3c29d0a3670a',
     averageRating: 0,
     totalReviews: 0,
     totalFollows: 0,
     totalConfirms: 0
   },
   {
-    id: 'eb3d9829-59f7-457f-8f09-beea8ca58dfc',
+    userId: 'eb3d9829-59f7-457f-8f09-beea8ca58dfc',
     averageRating: 0,
     totalReviews: 0,
     totalFollows: 0,
     totalConfirms: 0
   },
   {
-    id: '5441d657-f796-427a-b66d-cc5274c60c11',
+    userId: '5441d657-f796-427a-b66d-cc5274c60c11',
     averageRating: 0,
     totalReviews: 0,
     totalFollows: 0,
     totalConfirms: 0
   },
   {
-    id: '3ee03c9c-9ba5-43eb-a03d-fa7d3e546e41',
+    userId: '3ee03c9c-9ba5-43eb-a03d-fa7d3e546e41',
     averageRating: 0,
     totalReviews: 0,
     totalFollows: 0,
